afffe1070e3b0e54c65c4abaca9bcf891bd4c28c
kubhaniri <kubhaniri@gmail.com>
2020-09-16 16:59:52 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "kubha-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://3mbopdwbl6.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_tAXNlr4DK",
+      APP_CLIENT_ID: "5tih0drqn8c2vst196qtbmntuv",
+      IDENTITY_POOL_ID: "us-east-2:791dce0e-8168-4d85-a756-2fe8c2c73659"
+    }
+  };
